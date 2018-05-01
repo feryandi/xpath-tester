8a3f6732ecf92fa360aab120c1565877f806d17e
feryandi <fnurdiantoro@traveloka.com>
2018-05-01 18:03:13 +0700

    The program

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # xpath
 Painless XPath tester for your need
+
+Just type something like
+`python xpath.py --url google.com --xpath //div`
+
+No need for any dependency.
+
+Should works for both Python 2 and 3.
---
+++ xpath.py
@@ -0,0 +1,48 @@
+from lxml import etree
+import argparse
+import sys
+
+try: #python3
+  import urllib.request as pyurllib
+except: #python2
+  import urllib2 as pyurllib
+
+def parse_arguments(argv):
+  parser = argparse.ArgumentParser(description='XPath tester')
+  parser.add_argument('--url')
+  parser.add_argument('--html')
+  parser.add_argument('--xpath')
+  return parser.parse_args(argv)
+
+def main(argv):
+  args = parse_arguments(argv)
+
+  if args.xpath == None:
+    print('What XPath that you need to try?')
+    return
+
+  print(args)
+
+  html_data = args.html
+
+  if html_data == None:
+    if (args.url != None):
+      print('Start URL fetch')
+      url = args.url
+      if url[:4] != 'http':
+        url = 'http://' + url
+      html_data = pyurllib.urlopen(url).read()
+    else:
+      print('Please insert --html or --url')
+      return
+  
+  tree = etree.HTML(html_data)
+  results = tree.xpath(args.xpath)
+
+  for result in results:
+    print(etree.tostring(result, pretty_print=True))
+    print('--- --- --- --- --- ---')
+    
+if __name__ == "__main__":
+  main(sys.argv[1:])
+
